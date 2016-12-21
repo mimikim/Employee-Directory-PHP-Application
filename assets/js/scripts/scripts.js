c62bcb6dfccd20964c2c2081189bd915c0f401f3
Mimi Kim <mimikimwebdeveloper@gmail.com>
2016-12-21 00:26:26 -0500

    updating assets folder

--- assets/js/scripts/scripts.js
+++ assets/js/scripts/scripts.js
@@ -1,16 +1,18 @@
 jQuery(document).ready(function($){
 
-    $('#employee-table').DataTable( {
-        paging: true,
-        "order": []
-        //scrollY: 600
-    } );
 
     $('#users-table').DataTable( {
         paging: true,
         "order": []
         //scrollY: 600
     } );
+
+    var table = $('#employee-table').DataTable({
+        paging: true,
+        buttons: [ 'copy', 'excel', 'pdf' ],
+        "order": [[ 1, "asc" ]]
+    });
+    table.buttons( 0, null ).containers().appendTo('.table-buttons');
 
 
     $("button").click(function(){
