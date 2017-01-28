3e81d98bb272446111ce07d339ccac080f9d7d51
Mimi Kim <mimikim@users.noreply.github.com>
2017-01-28 00:32:49 -0500

    heroku config file

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+// defines database connection constants
+define('DB_HOST','us-cdbr-iron-east-04.cleardb.net');
+define('DB_USER','bc2faa85f54cd8');
+define('DB_PASS','9118a4db');
+define('DB_NAME','heroku_468050f6bcb96dc');
